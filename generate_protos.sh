d2d81da70dc6db14cb39c61e5926746cbb904baa
greg <greg.micah.jensen@gmail.com>
2026-09-14 17:20:57 -0400

    globs protos in generation script

--- generate_protos.sh
+++ generate_protos.sh
@@ -1,5 +1,3 @@
-protos=("echo")
-
-for proto in "${protos[@]}"; do
-    python3 -m grpc_tools.protoc -I. --python_out=src --pyi_out=src --grpc_python_out=src protos/$proto.proto
+for proto in protos/*.proto; do
+    python3 -m grpc_tools.protoc -I. --python_out=src --pyi_out=src --grpc_python_out=src "$proto"
 done
