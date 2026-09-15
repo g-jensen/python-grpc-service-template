9d49357f3096986d0df4b6650769f93033cf87eb
greg <greg.micah.jensen@gmail.com>
2026-09-15 18:36:55 -0400

    adds typehint to example servicer

--- src/servicers/echo/servicer.py
+++ src/servicers/echo/servicer.py
@@ -6,5 +6,5 @@ class EchoServicer(echo_pb2_grpc.EchoServicer):
     def __init__(self):
         return
     
-    def Call(self, request, context):
+    def Call(self, request: echo_pb2.EchoRequest, context):
         return echo_pb2.EchoResponse(content=request.content)
